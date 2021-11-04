76999fa666434e971bb134f9e50764dc7e7f68e4
Henry Park <huhushow@gmail.com>
2021-11-03 21:07:56 -0700

    set valid args
    set args for the assessment and register script to run

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const coinChanger = require('./coinChanger');
 
-let test = new coinChanger('1,2,3', 10);
+let test = new coinChanger([1, 2, 5, 10, 20, 50, 100, 200], 500);
 console.log(test.calculate());
