ceea39631c829d2d7295c2a7a2dbc270df418d70
Henry Park <huhushow@gmail.com>
2021-11-04 21:25:47 -0700

    add more test

--- index.test.js
+++ index.test.js
@@ -17,3 +17,25 @@ describe("args validation", () => {
     expect(() => new coinChanger([1, 2, 5], '10')).toThrow(TypeError);
   });
 });
+
+describe("run numbers", () => {
+  const type = [1,2,5];
+  const amounts = {
+    1: 1,
+    2: 2,
+    3: 2,
+    4: 3,
+    5: 4,
+    6: 5,
+    7: 6,
+    8: 7,
+    9: 8,
+    10: 10
+  };
+  for (let i in amounts) {
+    let c = new coinChanger(type, parseInt(i));
+    test(`bill amount ${i}`, () => {
+      expect(c.calculate()).toBe(amounts[i]);
+    });
+  }
+});
